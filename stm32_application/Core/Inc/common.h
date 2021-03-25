fad2b1688b28907b4a01a98fd121b54003c3a672
dzhang <daniel.zhang@aviatnet.com>
2021-03-25 23:08:32 +1300

    Remove suart IAP

--- stm32_application/Core/Inc/common.h
+++ stm32_application/Core/Inc/common.h
@@ -59,44 +59,6 @@ extern "C" {
 #define APP_DEFAULT_ADD 0x8005000
 typedef  void (*pFunction)(void);
     
-typedef enum
-{
-    IAP_ERROR = 0,
-    IAP_SUCCESS,
-
-    IAP_ERROR_HEAD_INVALID,
-    IAP_ERROR_FRAME_INVALID,
-    IAP_ERROR_CHKSUM_INVALID,
-    
-    // common
-    IAP_CMD_CONNECT,
-    IAP_CONNECT_SUCCESS,
-    IAP_CONNECT_FAIL,
-    
-    // SD card programming
-    IAP_CMD_SETARGS,
-    IAP_SETARGS_SUCCESS,
-    IAP_SETARGS_FAIL,
-
-    IAP_CMD_BUFFER,
-    IAP_BUFFER_SUCCESS,
-    IAP_BUFFER_FAIL,
-
-    IAP_CMD_CLEAR,
-    IAP_CLEAR_SUCCESS,
-    IAP_CLEAR_FAIL,
-
-    IAP_CMD_SAVE,
-    IAP_SAVE_SUCCESS,
-    IAP_SAVE_FAIL,
-
-    IAP_CMD_REBOOT,
-    IAP_REBOOT_SUCCESS,
-    IAP_REBOOT_FAIL,
-    
-    IAP_MAX_COMMAND = 100,
-} IAP_COMMANDS;
-
 typedef struct
 {
     GPIO_TypeDef *port;

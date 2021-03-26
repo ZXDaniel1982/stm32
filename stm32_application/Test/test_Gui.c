470e0197befdc0771343c658449e36f03c369225
Dzhang <daniel.zhang@aviatnet.com>
2021-03-26 18:26:14 +1300

    Get font

--- stm32_application/Test/test_Gui.c
+++ stm32_application/Test/test_Gui.c
@@ -4,14 +4,23 @@
 #include "lv_port_disp.h"
 #include "lv_port_indev.h"
 
+#define PIC 0
+
 void LCD_Fill(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, lv_color_t *color)
 {
 	for (int i=y1; i<=y2; i++) {
 		for (int j=x1; j<=x2; j++) {
-			if (color->full == 0xffff)
+#if (PIC == 1)
+			if (color->full == 0xef7e)
+				printf(".");
+			else if (color->full == 0xffff)
 				printf(" ");
 			else
 				printf("*");
+#else
+            if ((i < 16) && (j<11))
+                printf("0x%04x, ", color->full);
+#endif
 			color++;
 		}
 		printf("\n");
@@ -23,17 +32,24 @@ void main()
 	lv_init();
     lv_port_disp_init();
 
-    lv_obj_t * btn = lv_btn_create(lv_scr_act(), NULL);     /*Add a button the current screen*/
-    lv_obj_set_pos(btn, 10, 10);                            /*Set its position*/
-    lv_obj_set_size(btn, 120, 50);                          /*Set its size*/
-
-    lv_obj_t * label = lv_label_create(btn, NULL);          /*Add a label to the button*/
-    lv_label_set_text(label, "Button");                     /*Set the labels text*/
-
+    static lv_style_t label_shadow_style;
+    lv_style_init(&label_shadow_style);
+    
+    lv_style_set_text_opa(&label_shadow_style, LV_STATE_DEFAULT, LV_OPA_50);
+    lv_style_set_text_color(&label_shadow_style, LV_STATE_DEFAULT, LV_COLOR_RED);
+    
+    lv_obj_t * shadow_label = lv_label_create(lv_scr_act(), NULL);
+    lv_obj_add_style(shadow_label, LV_LABEL_PART_MAIN, &label_shadow_style);
+    
     lv_obj_t * label1 = lv_label_create(lv_scr_act(), NULL);
-    lv_label_set_text(label1, "Hello world!");
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-    lv_obj_align(btn, label1, LV_ALIGN_OUT_TOP_MID, 0, -10);
+    lv_label_set_long_mode(label1, LV_LABEL_LONG_BREAK);
+    lv_label_set_text(label1, "N");
+    
+    lv_label_set_text(shadow_label, lv_label_get_text(label1));
+    
+    lv_obj_set_pos(label1, 0, 0);
+    
+    lv_obj_align(shadow_label, label1, LV_ALIGN_IN_TOP_LEFT, 1, 1);
 
     lv_task_handler();
 }
